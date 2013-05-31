98450869df7df2876f4cf17befa65b58e75d84ca
Erick Fleming <erick@sudogs.com>
2013-05-31 10:11:58 -0500

    added sanity check test case

---
+++ src/test/client/main.js
@@ -0,0 +1,11 @@
+var assert = chai.assert
+
+describe("main", function () {
+
+    var utils = lymph.require("lymph-core").utils
+
+    it("has a sanity check", function () {
+        assert.equal(true, true)
+    })
+})
+
