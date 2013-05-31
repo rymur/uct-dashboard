16496edd5e97ff2adb914ba068ab2f583578a3bd
Erick Fleming <erick@sudogs.com>
2013-05-31 10:45:30 -0500

    refactored main to use lymph define

--- src/main/client/main.js
+++ src/main/client/main.js
@@ -1,206 +1,210 @@
-function create (definition) {
-    var defaultProto = {
-        isCool: function () {
-            return "coo"
+lymph.define("main", function (require) {
+
+    function create (definition) {
+        var defaultProto = {
+            isCool: function () {
+                return "coo"
+            }
+        }
+
+        return Object.create(defaultProto, definition)
+    }
+
+    function ListItemFormPresenter (model, view) {
+
+        view.setContent("")
+
+        $(view).on("added", function (e, newItem) {
+            var lim = listItemModel()
+            lim.text = newItem.text
+            model.add(lim)
+        })
+
+        this.renderTo = function (container) {
+            container.append(view.$el)
         }
     }
 
-    return Object.create(defaultProto, definition)
-}
+    function listItemFormView () {
 
-function ListItemFormPresenter (model, view) {
+        var view = {}
 
-    view.setContent("")
+        var content = $("<input>")
+        var button = $("<button>Add Item</button>")
 
-    $(view).on("added", function (e, newItem) {
-        var lim = listItemModel()
-        lim.text = newItem.text
-        model.add(lim)
-    })
+        view.$el = $('<div></div>') 
 
-    this.renderTo = function (container) {
-        container.append(view.$el)
-    }
-}
+        view.$el.append(content)
+        view.$el.append(button)
 
-function listItemFormView () {
-
-    var view = {}
-
-    var content = $("<input>")
-    var button = $("<button>Add Item</button>")
-
-    view.$el = $('<div></div>') 
-
-    view.$el.append(content)
-    view.$el.append(button)
-
-    view.setContent = function (text) {
-        content.val(text)
-    }
-
-    $("button", view.$el).on("click", function (e) {
-        $(view).trigger("added", { text: content.val() })
-    })
-
-    return view
-}
-
-function listModel () {
-
-    var items = []
-    var model = {}
-
-    model.add = function (newItem) {
-        items.push(newItem)
-        $(model).trigger("change", newItem)
-    }
-
-    model.each = function (fn) {
-        items.forEach(fn)
-    }
-
-    return model
-}
-
-function listView () {
-
-    var view = {}
-
-    view.$el = $("<ul></ul>") 
-
-    view.addItem = function (itemView) {
-        view.$el.append(itemView.$el)
-    }
-
-    return view
-}
-
-function ListPresenter (model, view) {
-
-    // add any existing items
-    model.each(function (x) {
-        view.addItem(x)
-    })
-
-    // listen to the model's change events
-    $(model).on("change", function (e, lim) {
-        var liv = listItemView()
-        var lip = new ListItemPresenter(lim, liv)
-        view.addItem(liv)
-    })
-
-    this.renderTo = function (container) {
-        container.append(view.$el)
-    }
-
-    
-}
-
-function listItemModel () {
-
-    var text = ""
-    var model = {}
-
-    Object.defineProperty(model, "text", {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-            return text
-        },
-        set: function (newValue) {
-            text = newValue
-            $(model).trigger("change", newValue)
+        view.setContent = function (text) {
+            content.val(text)
         }
-    })
 
-    return model
-}
+        $("button", view.$el).on("click", function (e) {
+            $(view).trigger("added", { text: content.val() })
+        })
 
-function listItemView () {
-
-    var view = {}
-
-    view.$el = $("<li></li>")
-
-    view.setContent = function (content) {
-        view.$el.html(content)
+        return view
     }
 
-    return view
-}
+    function listModel () {
 
-function ListItemPresenter (model, view) {
+        var items = []
+        var model = {}
 
-    view.setContent(model.text)
+        model.add = function (newItem) {
+            items.push(newItem)
+            $(model).trigger("change", newItem)
+        }
 
-    this.renderTo = function (container) {
-        container.append(view.$el)
+        model.each = function (fn) {
+            items.forEach(fn)
+        }
+
+        return model
     }
-}
 
-function titleModel (defaults) {
-    defaults = defaults || { content: "" }
+    function listView () {
 
-    var content = defaults.content
-    var model = {}
+        var view = {}
 
-    Object.defineProperty(model, "content", {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-            return content
-        },
-        set: function (newValue) {
-            content = newValue
-            $(model).trigger("change", newValue)
+        view.$el = $("<ul></ul>") 
+
+        view.addItem = function (itemView) {
+            view.$el.append(itemView.$el)
         }
-    })
 
-    return model
-}
+        return view
+    }
 
-function titleView () {
+    function ListPresenter (model, view) {
 
-    var view = {}
+        // add any existing items
+        model.each(function (x) {
+            view.addItem(x)
+        })
 
-    view.$el = $("<h1></h1>") 
+        // listen to the model's change events
+        $(model).on("change", function (e, lim) {
+            var liv = listItemView()
+            var lip = new ListItemPresenter(lim, liv)
+            view.addItem(liv)
+        })
 
-    Object.defineProperty(view, "content", {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-            return this.$el.html()
-        },
-        set: function (newValue) {
-            this.$el.html(newValue)
+        this.renderTo = function (container) {
+            container.append(view.$el)
         }
-    })
 
-    view.$el.on("click", function () {
-        $(view).trigger("clicked")
-    })
+        
+    }
 
-    return view
-}
+    function listItemModel () {
 
-function TitlePresenter (model, view) {
+        var text = ""
+        var model = {}
 
-    view.content = model.content
+        Object.defineProperty(model, "text", {
+            enumerable: true,
+            configurable: true,
+            get: function () {
+                return text
+            },
+            set: function (newValue) {
+                text = newValue
+                $(model).trigger("change", newValue)
+            }
+        })
 
-    $(view).on("clicked", function () {
-        console.log("clicked")
-    })
+        return model
+    }
 
-    $(model).on("change", function () {
-        console.log("modelchanged", view)
+    function listItemView () {
+
+        var view = {}
+
+        view.$el = $("<li></li>")
+
+        view.setContent = function (content) {
+            view.$el.html(content)
+        }
+
+        return view
+    }
+
+    function ListItemPresenter (model, view) {
+
+        view.setContent(model.text)
+
+        this.renderTo = function (container) {
+            container.append(view.$el)
+        }
+    }
+
+    function titleModel (defaults) {
+        defaults = defaults || { content: "" }
+
+        var content = defaults.content
+        var model = {}
+
+        Object.defineProperty(model, "content", {
+            enumerable: true,
+            configurable: true,
+            get: function () {
+                return content
+            },
+            set: function (newValue) {
+                content = newValue
+                $(model).trigger("change", newValue)
+            }
+        })
+
+        return model
+    }
+
+    function titleView () {
+
+        var view = {}
+
+        view.$el = $("<h1></h1>") 
+
+        Object.defineProperty(view, "content", {
+            enumerable: true,
+            configurable: true,
+            get: function () {
+                return this.$el.html()
+            },
+            set: function (newValue) {
+                this.$el.html(newValue)
+            }
+        })
+
+        view.$el.on("click", function () {
+            $(view).trigger("clicked")
+        })
+
+        return view
+    }
+
+    function TitlePresenter (model, view) {
+
         view.content = model.content
-    })
 
-    this.renderTo = function (container) {
-        container.append(view.$el)
+        $(view).on("clicked", function () {
+            console.log("clicked")
+        })
+
+        $(model).on("change", function () {
+            console.log("modelchanged", view)
+            view.content = model.content
+        })
+
+        this.renderTo = function (container) {
+            container.append(view.$el)
+        }
     }
-}
+
+})
 
 $(document).ready(function () {
 
@@ -265,49 +269,51 @@ $(document).ready(function () {
 
 })
 
-    //var $body = $("body")
 
-    //listM = listModel()
 
-    //titleV = titleView()
+        //var $body = $("body")
 
-    //titleM = titleModel()
-    //titleM.content = "default"
+        //listM = listModel()
 
-    //titleP = new TitlePresenter(titleM, titleV)
-    //titleP.renderTo($body)
+        //titleV = titleView()
 
-    //listItemFormV = listItemFormView()
-    //listItemFormP = new ListItemFormPresenter(listM, listItemFormV)
-    //listItemFormP.renderTo($body)
+        //titleM = titleModel()
+        //titleM.content = "default"
 
-    //listV = listView()
-    //listP = new ListPresenter(listM, listV)
-    //listP.renderTo($body)
+        //titleP = new TitlePresenter(titleM, titleV)
+        //titleP.renderTo($body)
 
-    //var creds = {
-        //"account":"VUIIS_IVIS",
-        //"savegrp":"on",
-        //"user":"manager",
-        //"saveusr":"on",
-        //"pwd": null,
-        //"savepwd":"on",
-        //"passwd":"japon",
-        //"end":"0"
-    //}
+        //listItemFormV = listItemFormView()
+        //listItemFormP = new ListItemFormPresenter(listM, listItemFormV)
+        //listItemFormP.renderTo($body)
 
-    //jQuery.post("http://faces.ccrc.uga.edu/ccrcfaces/login.php", creds, function (data, statusText) {
-        //console.log(statusText, data)
-    //})
-//
-    //$("#login").on("click", function () {
+        //listV = listView()
+        //listP = new ListPresenter(listM, listV)
+        //listP.renderTo($body)
 
         //var creds = {
-            //"username": $("#username").val(),
-            //"password": $("#password").val()
+            //"account":"VUIIS_IVIS",
+            //"savegrp":"on",
+            //"user":"manager",
+            //"saveusr":"on",
+            //"pwd": null,
+            //"savepwd":"on",
+            //"passwd":"japon",
+            //"end":"0"
         //}
 
-        //jQuery.post("/cgi-bin/show_form.com", creds, function (data, statusText) {
-            //console.log(data)
-        //}, "json")
-    //})
+        //jQuery.post("http://faces.ccrc.uga.edu/ccrcfaces/login.php", creds, function (data, statusText) {
+            //console.log(statusText, data)
+        //})
+    //
+        //$("#login").on("click", function () {
+
+            //var creds = {
+                //"username": $("#username").val(),
+                //"password": $("#password").val()
+            //}
+
+            //jQuery.post("/cgi-bin/show_form.com", creds, function (data, statusText) {
+                //console.log(data)
+            //}, "json")
+        //})
