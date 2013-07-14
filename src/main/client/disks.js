39b4c9cf122b4fcb01f0e7b5abacfde3387aeeac
Erick Fleming <erick@sudogs.com>
2013-07-14 14:56:54 -0500

    ported tests to new lymph-based testing

--- src/main/client/disks.js
+++ src/main/client/disks.js
@@ -22,58 +22,9 @@ exports.buildItemView = function (data) {
         h.TD({ class:"number"}, bytesToSize(freeBytes)),
         h.TD({ class:"number"}, bytesToSize(maxBytes))
     )
-}
-function buildProgressView (percentage) {
-    return h.METER({ low: "80", high: "95", max: "100", value: percentage, title: percentage + "% Used" })
-}
 
-function percentageUsed (free, max) {
-    var used = max - free
-    return ((used / max ) * 100).toFixed(2)
-}
-
-function bytesToSize (bytes) {
-    var sizes = ['KB', 'MB', 'GB', 'TB']
-    if (bytes === 0) return 'n/a'
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 0)
-    if (i === 0) return bytes + ' ' + sizes[i] 
-    return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i]
-}
-
-function volume2name (vol) {
-    if (vol === "I64SYS") {
-        return "DISK0"
-    }
-    else if (vol === "IUSER_01") {
-        return "DISK1"
-    }
-    else if (/IDATA_02[A-T]/.test(vol)) {
-        return "DISK3"
-    }
-    else if (/IDATA_03[A-E]/.test(vol)) {
-        return "DISK4"
-    }
-    else if (/IDATA_04[A-L]/.test(vol)) {
-        return "DISK5"
-    }
-    else if (/IDATA_01[A-c]/.test(vol)) {
-        return "DISK2"
-    }
-    else {
-        return null
-    }
-}
-
-function hasVolumes (device) {
-    return device.volCount > 0
-}
-
-function transform (device) {
-    return {
-        name: volume2name(device.volName),
-        description: "",
-        free: device.free,
-        max: device.max
+    function buildProgressView (percentage) {
+        return h.METER({ low: "80", high: "95", max: "100", value: percentage, title: percentage + "% Used" })
     }
 }
 
@@ -95,8 +46,62 @@ exports.preProcess = function (rawData) {
         groupedArray.push(grouped[i])
     }
 
-    return groupedArray.sort(function (a, b) {
-        return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0
-    })
+    return groupedArray.sort(sortBy("name"))
+
+    function sortBy (name) {
+        return function (a, b) {
+            return (a[name] < b[name]) ? -1 : (a[name] > b[name]) ? 1 : 0
+        }
+    }
+
+    function hasVolumes (device) {
+        return device.volCount > 0
+    }
+
+    function transform (device) {
+        return {
+            name: volume2name(device.volName),
+            description: "",
+            free: device.free,
+            max: device.max
+        }
+    }
+
+    function volume2name (vol) {
+        if (vol === "I64SYS") {
+            return "DISK0"
+        }
+        else if (vol === "IUSER_01") {
+            return "DISK1"
+        }
+        else if (/IDATA_02[A-T]/.test(vol)) {
+            return "DISK3"
+        }
+        else if (/IDATA_03[A-E]/.test(vol)) {
+            return "DISK4"
+        }
+        else if (/IDATA_04[A-L]/.test(vol)) {
+            return "DISK5"
+        }
+        else if (/IDATA_01[A-c]/.test(vol)) {
+            return "DISK2"
+        }
+        else {
+            return null
+        }
+    }
+}
+
+function percentageUsed (free, max) {
+    var used = max - free
+    return ((used / max ) * 100).toFixed(2)
+}
+
+function bytesToSize (bytes) {
+    var sizes = ['KB', 'MB', 'GB', 'TB']
+    if (bytes === 0) return 'n/a'
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 0)
+    if (i === 0) return bytes + ' ' + sizes[i] 
+    return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i]
 }
 
--- src/test/scheduling.js
+++ src/test/scheduling.js
@@ -1,108 +1,96 @@
-//describe("scheduling", function () {
+var lymphTest = require("lymph-test")
+var assert = lymphTest.assert
 
-    //var assert = chai.assert
-    //var scheduling = lymph.require("scheduling")
+var scheduling = require("../main/client/scheduling")
 
-    //describe("processing raw data from server", function () {
+module.exports = lymphTest.suite("disks", function (test) {
 
-        //var rawData = [
-            //"40 2013-05-24 16:00:00 2013-05-24 17:00:00 fe_nf1 Jean nf1 col2 bone | N/A",
-            //"40 2013-05-15 17:00:00 2013-05-16 06:00:00 fe_nasa VBX | N/A",
-            //"40 2013-06-07 16:00:00 2013-06-08 21:00:00 orear_plasmin  | N/A"
-        //].join("\n")
+    var rawData = [
+        "40 2013-05-24 16:00:00 2013-05-24 17:00:00 fe_nf1 Jean nf1 col2 bone | N/A",
+        "40 2013-05-15 17:00:00 2013-05-16 06:00:00 fe_nasa VBX | N/A",
+        "40 2013-06-07 16:00:00 2013-06-08 21:00:00 orear_plasmin  | N/A"
+    ].join("\n")
 
-        //var data1 = [
-            //{ 
-                //scanner: "40",
-                //start: new Date("Fri May 24 2013 16:00:00 GMT-0500 (CDT)"),
-                //end: new Date("Fri May 24 2013 17:00:00 GMT-0500 (CDT)"),
-                //account: "fe_nf1",
-                //comment: "Jean nf1 col2 bone"
-            //},
-            //{ 
-                //scanner: "40",
-                //start: new Date("Wed May 15 2013 17:00:00 GMT-0500 (CDT)"),
-                //end: new Date("Thu May 16 2013 06:00:00 GMT-0500 (CDT)"),
-                //account: "fe_nasa",
-                //comment: "VBX"
-            //},
-            //{ 
-                //scanner: "40",
-                //start: new Date("Fri Jun 07 2013 16:00:00 GMT-0500 (CDT)"),
-                //end: new Date("Fri Jun 07 2013 21:00:00 GMT-0500 (CDT)"),
-                //account: "fe_nasa",
-                //comment: ""
-            //}
-        //]
+    var processedData = [//{{
+        { 
+             scanner: "40"
+            ,start: new Date("2013-05-24T16:00:00-0500")
+            ,end: new Date("2013-05-24T17:00:00-0500")
+            ,account: "fe_nf1"
+            ,comment: "Jean nf1 col2 bone"
+            ,part: "full"
+        },
+        { 
+             scanner: "40"
+            ,start: new Date("2013-05-15T17:00:00-0500")
+            ,end: new Date("2013-05-16T06:00:00-0500")
+            ,account: "fe_nasa"
+            ,comment: "VBX"
+            ,part: "full"
+        },
+        { 
+             scanner: "40"
+            ,start: new Date("2013-06-07T16:00:00-0500")
+            ,end: new Date("2013-06-08T21:00:00-0500")
+            ,account: "orear_plasmin"
+            ,comment: ""
+            ,part: "full"
+        }
+    ] //}}
 
-        //it("extract the fields", function () {
-            //var result = scheduling.process(rawData)
-            //assert.deepEqual(result[0], data1[0])
-        //})
+    test("extract the fields", function () {
+        var actual = scheduling.process(rawData)
+        assert.equals(actual, processedData)
+    })
 
-        //it("breaks multi day events into separate events", function () {
-            //var result = scheduling.separate(data1)
+    test("breaks multi day events into separate events", function () {
 
-            //var data2 = [
-                //{ 
-                    //scanner: "40",
-                    //start: new Date("Fri May 24 2013 16:00:00 GMT-0500 (CDT)"),
-                    //end: new Date("Fri May 24 2013 17:00:00 GMT-0500 (CDT)"),
-                    //account: "fe_nf1",
-                    //comment: "Jean nf1 col2 bone"
-                //},
-                //{ 
-                    //scanner: "40",
-                    //start: new Date("Wed May 15 2013 17:00:00 GMT-0500 (CDT)"),
-                    //end: new Date("Wed May 15 2013 23:59:59:999 GMT-0500 (CDT)"),
-                    //account: "fe_nasa",
-                    //comment: "VBX"
-                //},
-                //{ 
-                    //scanner: "40",
-                    //start: new Date("Thu May 16 2013 00:00:00 GMT-0500 (CDT)"),
-                    //end: new Date("Thu May 16 2013 06:00:00 GMT-0500 (CDT)"),
-                    //account: "fe_nasa",
-                    //comment: "VBX"
-                //},
-                //{ 
-                    //scanner: "40",
-                    //start: new Date("Fri Jun 07 2013 16:00:00 GMT-0500 (CDT)"),
-                    //end: new Date("Fri Jun 07 2013 21:00:00 GMT-0500 (CDT)"),
-                    //account: "fe_nasa",
-                    //comment: ""
-                //}
-            //]
+        var actual = scheduling.separate(processedData)
 
-            //console.log(result)
+        var expected = [
+             { 
+                 scanner: "40"
+                ,start: new Date("2013-05-24T16:00:00-0500")
+                ,end: new Date("2013-05-24T17:00:00-0500")
+                ,account: "fe_nf1"
+                ,comment: "Jean nf1 col2 bone"
+                ,part: "full"
+            }
+            ,{ 
+                 scanner: "40"
+                ,start: new Date("2013-05-15T17:00:00-0500")
+                ,end: new Date("2013-05-15T23:59:59.999-0500")
+                ,account: "fe_nasa"
+                ,comment: "VBX"
+                ,part: "begin"
+            }
+            ,{ 
+                 scanner: "40"
+                ,start: new Date("2013-05-16T00:00:00-0500")
+                ,end: new Date("2013-05-16T06:00:00-0500")
+                ,account: "fe_nasa"
+                ,comment: "VBX"
+                ,part: "end"
+            }
+            ,{ 
+                 scanner: "40"
+                ,start: new Date("2013-06-07T16:00:00-0500")
+                ,end: new Date("2013-06-07T23:59:59.999-0500")
+                ,account: "orear_plasmin"
+                ,comment: ""
+                ,part: "begin"
+            }
+            ,{ 
+                 scanner: "40"
+                ,start: new Date("2013-06-08T00:00:00-0500")
+                ,end: new Date("2013-06-08T21:00:00-0500")
+                ,account: "orear_plasmin"
+                ,comment: ""
+                ,part: "end"
+            }
+        ]
 
-            //assert.deepEqual(result, data2)
-        //})
-    //})
-
-    //describe("a weeks view", function () {
-
-        //it("has a label for each hour", function () {
-            //var view = scheduling.timeSlotLabels()
-            //var slots = view.querySelectorAll("div")
-            //assert.equal(slots.length, 49)
-            //assert.equal(view.childNodes[0].innerHTML, "&nbsp;")
-            //assert.equal(view.childNodes[1].innerHTML, "12am")
-            //assert.equal(view.childNodes[2].innerHTML, "&nbsp;")
-        //})
-
-        //it("has a label for the day of week and date", function () {
-            //var slots = scheduling.daySlotColumns(new Date(2013, 5, 3))
-            //assert.equal(slots[0].childNodes[0].innerHTML, "Monday 6/3")
-        //})
-
-        ////it("has 7 columns of time slots for each day of week", function () {
-            ////var slots = scheduling.daySlotColumns(new Date(2013, 5, 3))
-            ////assert.equal(slots.length, 7)
-            ////assert.equal(slots[0].querySelectorAll("div").length, 48)
-            ////assert.equal(slots[1].querySelectorAll("div").length, 48)
-        ////})
-
-    //})
-//})
+        assert.equals(actual, expected)
+    })
+})
 
