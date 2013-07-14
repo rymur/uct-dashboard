09ca09c7a9df6a0545b1972057c3c8e594d56a2b
Erick Fleming <erick@sudogs.com>
2013-07-14 16:12:47 -0500

    array comma styling

--- src/test/scheduling.js
+++ src/test/scheduling.js
@@ -6,9 +6,9 @@ var scheduling = require("../main/client/scheduling")
 module.exports = lymphTest.suite("disks", function (test) {
 
     var rawData = [
-        "40 2013-05-24 16:00:00 2013-05-24 17:00:00 fe_nf1 Jean nf1 col2 bone | N/A",
-        "40 2013-05-15 17:00:00 2013-05-16 06:00:00 fe_nasa VBX | N/A",
-        "40 2013-06-07 16:00:00 2013-06-08 21:00:00 orear_plasmin  | N/A"
+         "40 2013-05-24 16:00:00 2013-05-24 17:00:00 fe_nf1 Jean nf1 col2 bone | N/A"
+        ,"40 2013-05-15 17:00:00 2013-05-16 06:00:00 fe_nasa VBX | N/A"
+        ,"40 2013-06-07 16:00:00 2013-06-08 21:00:00 orear_plasmin  | N/A"
     ].join("\n")
 
     var processedData = [//{{
@@ -19,16 +19,16 @@ module.exports = lymphTest.suite("disks", function (test) {
             ,account: "fe_nf1"
             ,comment: "Jean nf1 col2 bone"
             ,part: "full"
-        },
-        { 
+        }
+        ,{ 
              scanner: "40"
             ,start: new Date("2013-05-15T17:00:00-0500")
             ,end: new Date("2013-05-16T06:00:00-0500")
             ,account: "fe_nasa"
             ,comment: "VBX"
             ,part: "full"
-        },
-        { 
+        }
+        ,{ 
              scanner: "40"
             ,start: new Date("2013-06-07T16:00:00-0500")
             ,end: new Date("2013-06-08T21:00:00-0500")
