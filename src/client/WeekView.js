42ae34f8ffd9aa7eaa8d1d66c5c9d0e48f2cb5e4
Erick Fleming <erick@sudogs.com>
2013-08-04 07:49:54 -0500

    restructured weekview to be cleaner

--- src/client/WeekView.js
+++ src/client/WeekView.js
@@ -1,8 +1,7 @@
-var _ = exports
-
 var lymphClient = require("lymph-client")
 var lymphUtils  = require("lymph-utils")
 var lymphDates  = require("lymph-dates")
+var WeekCalendar = require("./weekCalendar")
 
 var utils = lymphUtils.utils
 var arrays = lymphUtils.arrays
@@ -10,29 +9,29 @@ var arrays = lymphUtils.arrays
 var dates = lymphDates.dates
 var html = lymphClient.html
 
-var WeekCalendar = require("./weekCalendar")
+var cols = utils.partial(divs, 7)
+var rows = utils.partial(divs, 24)
+var hourLabel = utils.compose(html.SPAN, dates.humanHour)
+var weekRowLabels = html.DIV({class:"day-label"},
+    rows(staticAttr({class:"hour"}), hourLabel))
 
-_.create = function () {
+exports.create = function () {
 
     var sd = WeekCalendar.startDate(2013, 7)
 
     var container = html.DIV({class:"f-75"},
-        _.weekColLabels(sd), _.weekRowLabels, _.weekCells(sd))
+        weekColLabels(sd), weekRowLabels, weekCells(sd))
 
     return container
 }
 
-_.divs = function (top, attrFn, childenFn) {
+function divs (top, attrFn, childenFn) {
     return arrays.map(function (x) {
         return html.DIV(attrFn(x), childenFn(x))
     }, arrays.range(0, top))
 }
 
-_.cols = utils.partial(_.divs, 7)
-
-_.rows = utils.partial(_.divs, 24)
-
-_.dayLabel = function (initialDate) {
+function dayLabel (initialDate) {
     return function (day) {
         var od = dates.addDays(initialDate, day)
         return dates.dayName((day === 6) ? 0 : day + 1) +
@@ -40,11 +39,9 @@ _.dayLabel = function (initialDate) {
     }
 }
 
-_.hourLabel = utils.compose(html.SPAN, dates.humanHour)
-
-_.weekCells = function (date) {
-    return _.cols(
-        colAttr, utils.partial(_.rows, _.staticAttr({class:"time"}), html.space))
+function weekCells (date) {
+    return cols(
+        colAttr, utils.partial(rows, staticAttr({class:"time"}), html.space))
 
     function colAttr(x) {
         var od = dates.addDays(date, x)
@@ -52,26 +49,23 @@ _.weekCells = function (date) {
     }
 }
 
-_.staticAttr = function (x) {
+function staticAttr (x) {
     return function () {
         return x
     }
 }
 
-_.weekRowLabels = html.DIV({class:"day-label"},
-    _.rows(_.staticAttr({class:"hour"}), _.hourLabel))
-
-_.weekColLabels = function (date) {
+function weekColLabels (date) {
     return html.DIV({class:"week-col-label"},
         html.DIV({class:"day-title"}, html.space()),
-        _.divs(7, _.staticAttr({class:"day-title"}), _.dayLabel(date)))
+        divs(7, staticAttr({class:"day-title"}), dayLabel(date)))
 }
 
 exports.suite = function (test, assert) {
 
     test("create a set of divs column for each day of the week", function () {
-        var attr = exports.staticAttr({class:"foo"})
-        var r1 = exports.cols(attr, function (x) { return "" })
+        var attr = staticAttr({class:"foo"})
+        var r1 = cols(attr, function (x) { return "" })
         assert.equals(r1.length, 7)
         assert.equals(r1[0].tagName, "DIV")
         assert.equals(r1[0].className, "foo")
@@ -79,8 +73,8 @@ exports.suite = function (test, assert) {
     })
 
     test("creates a set of divs for each hour of a day", function () {
-        var attr = exports.staticAttr({class:"foo"})
-        var r1 = exports.rows(attr, function (x) { return "" })
+        var attr = staticAttr({class:"foo"})
+        var r1 = rows(attr, function (x) { return "" })
         assert.equals(r1.length, 24)
         assert.equals(r1[1].tagName, "DIV")
         assert.equals(r1[1].className, "foo")
@@ -88,7 +82,7 @@ exports.suite = function (test, assert) {
     })
 
     test("create an hour label", function () {
-        var r1 = exports.hourLabel(1)
+        var r1 = hourLabel(1)
         assert.equals(r1.innerHTML, "1am")
         assert.equals(r1.tagName, "SPAN")
     })
